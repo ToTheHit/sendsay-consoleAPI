382713e17c3a8c555c9c83e393e1819c1fad0ff1
ToTheHit <dmtshkv@gmail.com>
2020-07-24 10:27:51 +0300

    Added function for compare actions

--- brew/src/components/Work/Editor/Editor.jsx
+++ brew/src/components/Work/Editor/Editor.jsx
@@ -53,6 +53,26 @@ const Editor = () => {
     setRequestContent(pretty);
   }
 
+  function compareObjects(obj1, obj2) {
+    for (let p in obj1) {
+      if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+      switch (typeof (obj1[p])) {
+        case 'object':
+          if (!Object.compare(obj1[p], obj2[p])) return false;
+          break;
+        case 'function':
+          if (typeof (obj2[p]) === 'undefined' || (p !== 'compare' && obj1[p].toString() !== obj2[p].toString())) return false;
+          break;
+        default:
+          if (obj1[p] !== obj2[p]) return false;
+      }
+    }
+    for (let p in obj2) {
+      if (typeof (obj1[p]) === 'undefined') return false;
+    }
+    return true;
+  };
+
   function sendAction(action) {
     if (isSending) return;
     setIsSending(true);
@@ -81,7 +101,7 @@ const Editor = () => {
       .finally(() => {
         const temp = actions;
         for (let i = 0; i < temp.length; i += 1) {
-          if (temp[i].action === obj.action) {
+          if (compareObjects(JSON.parse(temp[i].content), obj)) {
             temp.splice(i, 1);
             break;
           }
